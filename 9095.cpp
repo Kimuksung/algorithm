442134acf229a335c27d120748d9cbdd1f93b8fe
kimuksung2 <kimuksung2@gmail.com>
2020-02-27 18:04:43 +0900

    9095

---
+++ 9095.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+#include <vector>
+int arr[100];
+
+int answer(int temp2) {
+	int answer2;
+	vector<int> temp;
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[3] = 4;
+
+	for (int i = 4;i <= temp2;i++) {
+		arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+	}
+	return arr[temp2];
+}
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0;i < n;i++) {
+		int temp;
+		cin >> temp;
+
+		cout<<answer(temp)<<endl;
+	}
+
+
+	return 0;
+}
