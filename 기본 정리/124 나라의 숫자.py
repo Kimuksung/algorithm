34263dccf7bc598e39550a5ffa5c6b99dbe58e05
kimuksung2 <kimuksung2@gmail.com>
2020-11-18 18:49:54 +0900

    추가 코멘트 더 빨리 푸는법

--- 기본 정리/124 나라의 숫자.py
+++ 기본 정리/124 나라의 숫자.py
@@ -37,3 +37,25 @@ def solution(n):
 n= 500000000
 #change_str('200')
 solution(n)
+
+def solution(n):
+    answer = ''
+    c = 0
+    while True:
+        c+= 1
+        x = (n -1 ) % (3**c)
+        if x < 3 **(c-1):
+            answer += '1'
+        elif x<3 **(c-1) *2:
+            answer += '2'
+        else: 
+            answer += '4'
+        
+        n -= 3 ** c
+        if n <= 0 :
+            break
+        
+    return answer[::-1]
+    
+solution(15)
+
