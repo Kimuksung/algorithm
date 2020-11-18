6ea78af7c2fe8e5bfd9c4845c01f99661b6de93b
kimuksung <kimuksung2@daum.net>
2020-11-18 15:01:49 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12899

---
+++ 기본 정리/124 나라의 숫자.py
@@ -0,0 +1,39 @@
+temp_list = [ "4" , "0" , "1", "0", "2"] # change 
+
+def third_base(n): #3진법 표현
+    remainder = n % 3
+    answer =""
+    answer = str(remainder)+answer
+    share = n//3
+    while(share != 0):
+        remainder = share % 3
+        share = share//3        
+        answer = str(remainder)+answer
+    return answer
+
+def change_str( temp ): # 4-> 2-> 1 변경
+    if temp[0] == "0":
+        temp =  temp[1:]
+    temp_str = ""
+    for index , value in enumerate(temp):
+        if value == "0":
+            temp_str = temp_str[:-2] + temp_list[int(temp[index-1])] + temp_list[int(temp[index])]+temp[index+1:]
+            return change_str(temp_str)
+        else:
+            temp_str = temp_str + value
+        
+    temp = temp_str
+    return temp
+    
+def solution(n):
+    answer = ''
+    
+    #change 3진법
+    change_thing = third_base(n)
+    answer = change_str(change_thing)
+    
+    return answer
+
+n= 45
+solution(n)
+
