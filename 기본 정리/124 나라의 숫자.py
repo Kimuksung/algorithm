615eeeb0356bae2bd110dcf17f4a6af9e439c8ef
kimuksung2 <kimuksung2@gmail.com>
2020-11-18 15:57:45 +0900

    하나 시간초과 뜨는데 O(n) 에서 더 줄여야하는데 모르겟다

--- 기본 정리/124 나라의 숫자.py
+++ 기본 정리/124 나라의 숫자.py
@@ -13,11 +13,11 @@ def third_base(n): #3진법 표현
 
 def change_str( temp ): # 4-> 2-> 1 변경
     if temp[0] == "0":
-        temp =  temp[1:]
+        return change_str(temp[1:])
     temp_str = ""
     for index , value in enumerate(temp):
         if value == "0":
-            temp_str = temp_str[:-2] + temp_list[int(temp[index-1])] + temp_list[int(temp[index])]+temp[index+1:]
+            temp_str = temp_str[:-1] + temp_list[ int(temp[index-1])] + temp_list[int(temp[index]) ] + temp[index+1:]
             return change_str(temp_str)
         else:
             temp_str = temp_str + value
@@ -34,6 +34,6 @@ def solution(n):
     
     return answer
 
-n= 45
+n= 500000000
+#change_str('200')
 solution(n)
-
